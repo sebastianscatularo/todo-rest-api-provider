6d818b33ed8b5b2549d3ef332f2fab7169499f2f
Sebastian Scatularo <sebastianscatularo@gmail.com>
2015-04-20 08:53:33 -0300

    Delete some files

--- src/main/webapp/app/Dashboard.js
+++
@@ -1,8 +0,0 @@
-define(['handlebars', 'jquery'], function(handlebars, $) {
-    return {
-        show: function() {
-            console.log("Hola");
-            $('body').append("<h1> hello </h1>");
-        }
-    }
-});
